d76d2b1eaf236cd3e8f1e51655b809fb7992520f
Keeeyaan <keanjieden@gmail.com>
2023-08-24 12:00:06 +0800

    Added scroll to the top when navigating new pages

--- frontend/psits_frontend/src/components/Wrapper.tsx
+++ frontend/psits_frontend/src/components/Wrapper.tsx
@@ -3,10 +3,11 @@ import React, { useEffect } from 'react'
 const Wrapper = ({ children, title }: {children: React.ReactNode, title: string}) => {
   useEffect(() => {
     document.title = title; // Update the document title
+    window.scrollTo(0, 0); 
   }, [title]);
 
   return (
-    <section className=''>
+    <section className='mx-20'>
       {children}
     </section>
   )
