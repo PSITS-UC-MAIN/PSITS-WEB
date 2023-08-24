0cd00c66a068bad604f931d2294035c935adac5e
Keeeyaan <keanjieden@gmail.com>
2023-08-24 11:56:11 +0800

    Leverage psits_banner2 to the slider

--- frontend/psits_frontend/src/constants/index.ts
+++ frontend/psits_frontend/src/constants/index.ts
@@ -1,4 +1,4 @@
-import { psits_banner } from "@/assets";
+import { psits_banner, psits_banner2 } from "@/assets";
 
 export const homeBannerImages = [
   {
@@ -7,7 +7,7 @@ export const homeBannerImages = [
   },
   {
     name: "psits_banner2",
-    banner: psits_banner,
+    banner: psits_banner2,
   },
   {
     name: "psits_banner3",
@@ -16,20 +16,21 @@ export const homeBannerImages = [
 ];
 
 export const aboutBannerImages = [
-    {
-      name: "psits_banner",
-      banner: psits_banner,
-    },
-    {
-      name: "psits_banner2",
-      banner: psits_banner,
-    },
-    {
-      name: "psits_banner3",
-      banner: psits_banner,
-    },
-]
+  {
+    name: "psits_banner",
+    banner: psits_banner,
+  },
+  {
+    name: "psits_banner2",
+    banner: psits_banner2,
+  },
+  {
+    name: "psits_banner3",
+    banner: psits_banner,
+  },
+];
 
 export default {
-    homeBannerImages,aboutBannerImages
-}
+  homeBannerImages,
+  aboutBannerImages,
+};
