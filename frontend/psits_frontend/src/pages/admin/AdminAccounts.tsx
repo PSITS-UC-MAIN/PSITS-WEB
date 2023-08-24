cf37cfa988601d67f9840a77e33b76d252db71ac
Keeeyaan <keanjieden@gmail.com>
2023-08-25 01:25:21 +0800

    Added admin page components

---
+++ frontend/psits_frontend/src/pages/admin/AdminAccounts.tsx
@@ -0,0 +1,11 @@
+import Wrapper from "@/components/Wrapper";
+
+const AdminAccounts = () => {
+  return (
+    <Wrapper title="PSITS Admin | Accounts" noMargin>
+      <div className="h-screen">Accounts</div>
+    </Wrapper>
+  );
+};
+
+export default AdminAccounts;
