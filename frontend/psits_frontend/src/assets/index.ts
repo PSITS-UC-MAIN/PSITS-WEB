aa42d0424e8ed865b7d096fd7eca7ee780f505a4
Keeeyaan <keanjieden@gmail.com>
2023-08-24 18:48:54 +0800

    Added notfound image & trinitylogo

--- frontend/psits_frontend/src/assets/index.ts
+++ frontend/psits_frontend/src/assets/index.ts
@@ -1,4 +1,6 @@
 import psits_banner from "./psits_banner.jpg";
 import psits_banner2 from "./psits_banner2.png";
+import trinitylogo from "./trinitylogo.png";
+import notfound from "./not-found.svg";
 
-export { psits_banner, psits_banner2 };
+export { psits_banner, psits_banner2, trinitylogo, notfound };
