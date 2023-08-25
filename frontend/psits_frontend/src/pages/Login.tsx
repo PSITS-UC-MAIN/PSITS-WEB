827d3bc45b32a943b653fe0417a3475444782954
Keeeyaan <keanjieden@gmail.com>
2023-08-25 21:06:34 +0800

    Added validation

--- frontend/psits_frontend/src/pages/Login.tsx
+++ frontend/psits_frontend/src/pages/Login.tsx
@@ -1,6 +1,6 @@
-import * as z from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
@@ -10,20 +10,31 @@ import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import Wrapper from "@/components/Wrapper";
 
-const loginValidation = z.object({
+const LoginSchema = z.object({
   userId: z.string().min(8).max(15),
   password: z.string().min(6).max(30),
 });
 
+type LoginSchema = z.infer<typeof LoginSchema>;
+
 const Login = () => {
-  const form = useForm({
-    resolver: zodResolver(loginValidation),
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(LoginSchema),
   });
+
+  const onSubmit: SubmitHandler<LoginSchema> = (data) => {
+    //TODO: Send data to the server
+    console.log(data);
+  };
 
   return (
     <Wrapper title="PSITS | Login">
       <div className="w-[80%] m-auto bg-white flex flex-col justify-center lg:w-[550px] min-h-screen">
-        <form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Card>
             <CardHeader className="space-y-1">
               <CardTitle className="text-3xl mb-2 text-center">Log In</CardTitle>
@@ -31,17 +42,25 @@ const Login = () => {
             </CardHeader>
             <CardContent className="grid gap-4">
               <div className="grid gap-2">
-                <Label htmlFor="id">Student ID</Label>
-                <Input id="id" placeholder="I.D. No." />
+                <Label htmlFor="userId">Student ID</Label>
+                <Input autoComplete="off" id="userId" placeholder="I.D. No." {...register("userId")} />
+                {errors.userId && <p className="text-red-400 text-sm font-light">{errors.userId.message}</p>}
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" placeholder="Password" />
+                <Input
+                  autoComplete="off"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  {...register("password")}
+                />
+                {errors.password && <p className="text-red-400 text-sm font-light">{errors.password.message}</p>}
               </div>
               <div className="flex items-center space-x-2">
-                <Checkbox id="terms" />
+                <Checkbox id="remember" />
                 <label
-                  htmlFor="terms"
+                  htmlFor="remember"
                   className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 >
                   Remember me
@@ -54,13 +73,11 @@ const Login = () => {
               </Button>
               <Link to="/register">
                 <p className="mt-2 text-xs text-center text-gray-700">
-                  {" "}
                   Don't have an account yet? <span className=" text-blue-600 hover:underline">Register</span>
                 </p>
               </Link>
               <Link to="/resetpassword">
                 <p className="mt-2 text-xs text-center text-gray-700">
-                  {" "}
                   Forgot password? <span className=" text-blue-600 hover:underline">Reset Password</span>
                 </p>
               </Link>
