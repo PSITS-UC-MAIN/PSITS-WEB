294891c046ca4f9cbc7ac8fc7d4a06602017a640
Keeeyaan <keanjieden@gmail.com>
2023-08-26 12:37:05 +0800

    Added dummy data & functionality

--- frontend/psits_frontend/src/components/announcements/Announcement.tsx
+++ frontend/psits_frontend/src/components/announcements/Announcement.tsx
@@ -1,10 +1,33 @@
 import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { User } from "lucide-react";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
+
+const dummyData = [
+  {
+    id: "1234",
+    title: "Tidert Gaming",
+    creationDate: new Date(),
+    author: "kean",
+    content: "wassup mananap",
+    photo_img_links: "",
+  },
+  {
+    id: "12345",
+    title: "Need maney",
+    creationDate: new Date(),
+    author: "darelle",
+    content: "ayaw kol",
+    photo_img_links: "",
+  },
+];
 
 interface Announcement {
   id: string;
@@ -15,32 +38,77 @@ interface Announcement {
   photo_img_links: string;
 }
 
+const AnnouncementSchema = z.object({
+  title: z.string().min(4),
+  content: z.string().min(4),
+  image: z.any(),
+});
+
+type AnnouncementSchema = z.infer<typeof AnnouncementSchema>;
+
 const Announcement = ({ announcements }: { announcements: Announcement[] }) => {
   const [announceState, setAnnounceState] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AnnouncementSchema>({
+    resolver: zodResolver(AnnouncementSchema),
+  });
+
+  const onSubmit: SubmitHandler<AnnouncementSchema> = (data) => {
+    //TODO: Send data to the server
+    console.log(data);
+
+    const newData = {
+      id: (Math.random() * 1000).toString(),
+      title: data.title,
+      creationDate: new Date(),
+      author: "User",
+      content: data.content,
+      photo_img_links: "",
+    };
+
+    dummyData.push(newData);
+    setAnnounceState(false);
+  };
 
   return (
-    <div className="w-[60%]">
+    <div className="w-[70%]">
       {announceState ? (
-        <Card className="w-full">
-          <CardHeader>
-            <Input placeholder="Announcement Title" />
-          </CardHeader>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Card className="w-full">
+            <CardHeader>
+              <Input placeholder="Announcement Title" {...register("title")} />
+              {errors.title && <p className="text-red-400 text-sm font-light">{errors.title.message}</p>}
+            </CardHeader>
 
-          <CardContent>
-            <Textarea rows={5} className="bg-[#fafafa]" placeholder="Announce something to the students" />
-          </CardContent>
-
-          <CardFooter className="w-full flex items-center justify-between">
-            <Input type="file" accept="image/*" placeholder="Upload a photo" className="w-auto" />
-            <div className="flex items-center gap-4">
-              <Button variant="ghost" onClick={() => setAnnounceState(false)}>
-                {" "}
-                Cancel{" "}
-              </Button>
-              <Button className="bg-[#268EA7] hover:bg-[#3da7c2]"> Post </Button>
-            </div>
-          </CardFooter>
-        </Card>
+            <CardContent>
+              <Textarea
+                rows={5}
+                className="bg-[#fafafa]"
+                placeholder="Announce something to the students"
+                {...register("content")}
+              />
+              {errors.content && <p className="text-red-400 text-sm font-light">{errors.content.message}</p>}
+            </CardContent>
+            <CardFooter className="w-full flex items-center justify-between">
+              <Input
+                type="file"
+                accept="image/*"
+                placeholder="Upload a photo"
+                className="w-auto"
+                {...register("image")}
+              />
+              <div className="flex items-center gap-4">
+                <Button variant="ghost" onClick={() => setAnnounceState(false)}>
+                  Cancel
+                </Button>
+                <Button className="bg-[#268EA7] hover:bg-[#3da7c2]"> Post </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
       ) : (
         <div
           onClick={() => setAnnounceState(true)}
@@ -58,8 +126,8 @@ const Announcement = ({ announcements }: { announcements: Announcement[] }) => {
 
       <div className="border shadow rounded p-4">
         <h1 className="text-center font-bold text-3xl mb-4">Announcements</h1>
-        <div className="flex flex-col justify-center items-center">
-          {announcements.map((announcement) => {
+        <div className="flex flex-col justify-center items-center gap-4">
+          {dummyData.map((announcement) => {
             return (
               <Card className="w-[90%]" key={announcement.id}>
                 <CardHeader>
--- frontend/psits_frontend/src/components/events/Event.tsx
+++ frontend/psits_frontend/src/components/events/Event.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
-import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
@@ -15,57 +17,113 @@ import { Input } from "@/components/ui/input";
 interface Event {
   id: string;
   title: string;
-  creationDate: Date;
-  eventDate: Date;
+  creationDate?: Date;
+  eventDate?: Date;
   content: string;
   photo_img_links: string;
 }
+
+const dummyData: Event[] = [
+  {
+    id: "1234",
+    title: "Pakighimamat",
+    creationDate: new Date(),
+    eventDate: new Date(),
+    content: "wassup mananap",
+    photo_img_links: "",
+  },
+  {
+    id: "12345",
+    title: "SIR DD",
+    creationDate: new Date(),
+    eventDate: new Date(),
+    content: "ayaw kol",
+    photo_img_links: "",
+  },
+];
+
+const EventSchema = z.object({
+  title: z.string().min(4),
+  content: z.string().min(4),
+  image: z.any(),
+});
+
+type EventSchema = z.infer<typeof EventSchema>;
 
 const Event = ({ events }: { events: Event[] }) => {
   const [eventState, setEventState] = useState(false);
   const [date, setDate] = useState<Date>();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<EventSchema>({
+    resolver: zodResolver(EventSchema),
+  });
+
+  const onSubmit: SubmitHandler<EventSchema> = (data) => {
+    //TODO: Send data to the server
+    console.log(data);
+    console.log(date);
+    const newData = {
+      id: (Math.random() * 1000).toString(),
+      title: data.title,
+      creationDate: new Date(),
+      eventDate: date,
+      content: data.content,
+      photo_img_links: "",
+    };
+
+    dummyData.push(newData);
+    setDate(undefined);
+    setEventState(false);
+  };
+
   return (
-    <div className="w-[400px]">
+    <div className="w-[350px]">
       {eventState ? (
-        <Card className="w-full">
-          <CardHeader>
-            <Input placeholder="Event Title" />
-          </CardHeader>
-          <CardContent className="flex flex-col gap-4">
-            <Popover>
-              <PopoverTrigger asChild>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Card className="w-full">
+            <CardHeader>
+              <Input placeholder="Event Title" {...register("title")} />
+              {errors.title && <p className="text-red-400 text-sm font-light">{errors.title.message}</p>}
+            </CardHeader>
+            <CardContent className="flex flex-col gap-4">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    className={`justify-start text-left font-normal ${!date && "text-muted-foreground"}`}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {date ? format(date, "PPP") : <span>Pick a date</span>}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+                </PopoverContent>
+              </Popover>
+              <Textarea rows={4} className="bg-[#fafafa]" placeholder="Event description" {...register("content")} />
+              {errors.content && <p className="text-red-400 text-sm font-light">{errors.content.message}</p>}
+            </CardContent>
+            <CardFooter className="flex items-center justify-between gap-4">
+              <Input type="file" accept="image/*" placeholder="Upload a photo" {...register("image")} />
+              <div className="flex items-center gap-4">
                 <Button
-                  variant={"outline"}
-                  className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}
+                  variant="ghost"
+                  onClick={() => {
+                    setDate(undefined);
+                    setEventState(false);
+                  }}
                 >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date ? format(date, "PPP") : <span>Pick a date</span>}
+                  Cancel
                 </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0">
-                <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
-              </PopoverContent>
-            </Popover>
-            <Textarea rows={4} className="bg-[#fafafa]" placeholder="Event description" />
-          </CardContent>
-          <CardFooter className="flex items-center justify-between gap-4">
-            <Input type="file" accept="image/*" placeholder="Upload a photo" />
-            <div className="flex items-center gap-4">
-              <Button
-                variant="ghost"
-                onClick={() => {
-                  setDate(undefined);
-                  setEventState(false);
-                }}
-              >
-                {" "}
-                Cancel{" "}
-              </Button>
-              <Button className="bg-[#268EA7] hover:bg-[#3da7c2]"> Post </Button>
-            </div>
-          </CardFooter>
-        </Card>
+                <Button className="bg-[#268EA7] hover:bg-[#3da7c2]"> Post </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
       ) : (
         <div
           onClick={() => setEventState(true)}
@@ -82,18 +140,24 @@ const Event = ({ events }: { events: Event[] }) => {
       )}
       <div className="mt-4 border shadow p-4 rounded">
         <h1 className="text-center font-bold text-2xl mb-4">Upcoming Events</h1>
-        <Card className="w-full">
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
+        <div className="flex flex-col gap-4 items-center">
+          {dummyData.map((announcement) => {
+            return (
+              <Card className="w-[90%]" key={announcement.id}>
+                <CardHeader>
+                  <CardTitle>{announcement.title}</CardTitle>
+                  <CardDescription>{announcement.eventDate?.toISOString()}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p>{announcement.content}</p>
+                </CardContent>
+                <CardFooter>
+                  <p>{announcement.creationDate?.toISOString()}</p>
+                </CardFooter>
+              </Card>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
