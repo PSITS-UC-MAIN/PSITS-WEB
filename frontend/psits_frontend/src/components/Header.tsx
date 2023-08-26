7641dd3b0063de970866b29aee989baa19f891bd
gochuicod <gochuicodarelleq@gmail.com>
2023-08-27 00:44:36 +0800

    Added Order Summary Feature
    Added shopping cart item on the header which brings you to a modal
    with the summary of your ordered items after the button is clicked.

--- frontend/psits_frontend/src/components/Header.tsx
+++ frontend/psits_frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-
+import { psits_banner2 } from "@/assets";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -9,11 +9,71 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { ShoppingCart } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React, { useState } from "react";
+
+const dummyData = [
+  {
+    id: 1,
+    title: "I GYATT something in my eye T-Shirt",
+    price: 40,
+    photo_img_link: psits_banner2,
+    size: "S",
+    color: "Red",
+  },
+  {
+    id: 2,
+    title: "I GYATT something in my eye T-Shirt",
+    price: 40,
+    photo_img_link: psits_banner2,
+    size: "S",
+    color: "Red",
+  },
+  {
+    id: 3,
+    title: "I GYATT something in my eye T-Shirt",
+    price: 40,
+    photo_img_link: psits_banner2,
+    size: "S",
+    color: "Red",
+  },
+  {
+    id: 4,
+    title: "I GYATT something in my eye T-Shirt",
+    price: 40,
+    photo_img_link: psits_banner2,
+    size: "S",
+    color: "Red",
+  },
+  {
+    id: 5,
+    title: "I GYATT something in my eye T-Shirt",
+    price: 40,
+    photo_img_link: psits_banner2,
+    size: "S",
+    color: "Red",
+  },
+];
 
 const ROUTE = ["/admin", "/admin/accounts", "/admin/events", "/admin/merchandise"];
 
 const Header = () => {
   const { pathname } = useLocation();
+
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const calculateTotalPrice = () => {
+    let total = 0;
+    dummyData.forEach((item) => (total += item.price));
+    return total;
+  };
+
+  React.useEffect(() => {
+    const calculatedTotalPrice = calculateTotalPrice();
+    setTotalPrice(calculatedTotalPrice);
+  }, [dummyData]);
 
   if (ROUTE.includes(pathname)) return;
 
@@ -74,6 +134,51 @@ const Header = () => {
             </Button>
           </div>
           <div className="flex gap-4 items-center">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="link" className="relative">
+                  <ShoppingCart color="#fff" />
+                  <span className="absolute top-[-5px] end-[-5px] bg-red-600 text-white font-bold rounded-full p-1 text-[15px] px-3">
+                    8
+                  </span>
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[70%] bg-[#f9f9f9]">
+                <div className="flex flex-col mx-[3%]">
+                  <div className="grid grid-cols-4 text-center bg-[#254872] text-white rounded-md shadow-md p-5 mt-5 mb-5">
+                    <span>Product</span>
+                    <span>Size</span>
+                    <span>Color</span>
+                    <span>Price</span>
+                  </div>
+                  <ScrollArea className="h-[30em] w-full rounded-md">
+                    {dummyData.map((item) => (
+                      <div className="grid grid-cols-4 bg-white rounded-md shadow-md p-5 items-center mb-5">
+                        <div className="flex flex-cols gap-x-5 items-center">
+                          <img
+                            src={item.photo_img_link}
+                            alt="Product Image"
+                            className="w-[100px] h-[100px] rounded-md"
+                          />
+                          <span>{item.title}</span>
+                        </div>
+                        <span className="text-center">{item.size}</span>
+                        <span className="text-center">{item.color}</span>
+                        <span className="text-center">&#8369;{item.price}</span>
+                      </div>
+                    ))}
+                  </ScrollArea>
+                  <div className="flex flex-row justify-end items-center gap-x-10 p-5">
+                    <span className="bg-white rounded-md py-2 px-5 shadow-md">
+                      Order Total:&emsp;&#8369;{totalPrice}
+                    </span>
+                    <Button type="submit" className="bg-[#268EA7] hover:bg-[#3da7c2]">
+                      Checkout
+                    </Button>
+                  </div>
+                </div>
+              </DialogContent>
+            </Dialog>
             <Button className="text-md" variant="ghost" asChild>
               <Link to="/admin">Admin</Link>
             </Button>
