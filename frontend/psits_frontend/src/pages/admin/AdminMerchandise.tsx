15a262e420911dc398131868c86cf5d9f7983f84
Keeeyaan <keanjieden@gmail.com>
2023-08-27 01:23:42 +0800

    Formatted

--- frontend/psits_frontend/src/pages/admin/AdminMerchandise.tsx
+++ frontend/psits_frontend/src/pages/admin/AdminMerchandise.tsx
@@ -1,7 +1,11 @@
 import Wrapper from "@/components/Wrapper";
 
 const AdminMerchandise = () => {
-  return <Wrapper title="PSITS Admin | Merchandise" noMargin>Merchandise</Wrapper>;
+  return (
+    <Wrapper title="PSITS Admin | Merchandise" noMargin>
+      Merchandise
+    </Wrapper>
+  );
 };
 
 export default AdminMerchandise;
