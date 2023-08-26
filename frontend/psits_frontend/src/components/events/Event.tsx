3643bfceb759f7efab852a8847eed50d75123d53
Keeeyaan <keanjieden@gmail.com>
2023-08-27 01:27:32 +0800

    Leverage card UI

--- frontend/psits_frontend/src/components/events/Event.tsx
+++ frontend/psits_frontend/src/components/events/Event.tsx
@@ -5,20 +5,22 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
+import { psits_banner2 } from "@/assets";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { User } from "lucide-react";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
+import EventCard from "./EventCard";
 
 interface Event {
   id: string;
   title: string;
-  creationDate?: Date;
-  eventDate?: Date;
+  creationDate: Date;
+  eventDate: Date;
   content: string;
   photo_img_links: string;
 }
@@ -26,19 +28,19 @@ interface Event {
 const dummyData: Event[] = [
   {
     id: "1234",
-    title: "Pakighimamat",
-    creationDate: new Date(),
-    eventDate: new Date(),
+    title: "BONDING DAY",
+    creationDate: new Date("August 27, 2023 09:37:00"),
+    eventDate: new Date("August 27, 2023 09:37:00"),
     content: "wassup mananap",
-    photo_img_links: "",
+    photo_img_links: psits_banner2,
   },
   {
     id: "12345",
     title: "SIR DD",
-    creationDate: new Date(),
-    eventDate: new Date(),
+    creationDate: new Date("August 27, 2023 09:37:00"),
+    eventDate: new Date("August 27, 2023 09:37:00"),
     content: "ayaw kol",
-    photo_img_links: "",
+    photo_img_links: psits_banner2,
   },
 ];
 
@@ -66,11 +68,12 @@ const Event = ({ events }: { events: Event[] }) => {
     //TODO: Send data to the server
     console.log(data);
     console.log(date);
+
     const newData = {
       id: (Math.random() * 1000).toString(),
       title: data.title,
       creationDate: new Date(),
-      eventDate: date,
+      eventDate: new Date(),
       content: data.content,
       photo_img_links: "",
     };
@@ -81,7 +84,7 @@ const Event = ({ events }: { events: Event[] }) => {
   };
 
   return (
-    <div className="w-[350px]">
+    <div className="w-[70%] max-w-[350px] hidden lg:block">
       {eventState ? (
         <form onSubmit={handleSubmit(onSubmit)}>
           <Card className="w-full">
@@ -138,23 +141,19 @@ const Event = ({ events }: { events: Event[] }) => {
           <h1 className="text-gray-500 text-sm">Click me to add new event!</h1>
         </div>
       )}
-      <div className="mt-4 border shadow p-4 rounded">
+      <div className="mt-4 border shadow p-4 rounded bg-[#F9F9F9]">
         <h1 className="text-center font-bold text-2xl mb-4">Upcoming Events</h1>
         <div className="flex flex-col gap-4 items-center">
           {dummyData.map((announcement) => {
             return (
-              <Card className="w-[90%]" key={announcement.id}>
-                <CardHeader>
-                  <CardTitle>{announcement.title}</CardTitle>
-                  <CardDescription>{announcement.eventDate?.toISOString()}</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <p>{announcement.content}</p>
-                </CardContent>
-                <CardFooter>
-                  <p>{announcement.creationDate?.toISOString()}</p>
-                </CardFooter>
-              </Card>
+              <EventCard
+                key={announcement.id}
+                title={announcement.title}
+                eventDate={announcement.eventDate}
+                content={announcement.content}
+                photo_img_link={announcement.photo_img_links}
+                creationDate={announcement.creationDate}
+              />
             );
           })}
         </div>
