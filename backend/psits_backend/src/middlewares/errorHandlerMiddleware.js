58729818d6b41f275272a54ad9d11958abb68b6e
Keeeyaan <keanjieden@gmail.com>
2023-08-30 00:41:14 +0800

    [BACKEND] Change msg to message

--- backend/psits_backend/src/middlewares/errorHandlerMiddleware.js
+++ backend/psits_backend/src/middlewares/errorHandlerMiddleware.js
@@ -4,7 +4,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   console.log(err);
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const msg = err.message || "something went wrong, try again later";
-  res.status(statusCode).json({ msg: msg });
+  res.status(statusCode).json({ message: msg });
 };
 
 export default errorHandlerMiddleware;
