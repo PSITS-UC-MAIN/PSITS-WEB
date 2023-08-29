ed1b56d45b6a7c73ef9ba77f523cfc22c61800b8
JayMar921 <jayharronabejar@gmail.com>
2023-08-29 20:07:39 +0800

    [FRONTEND] Office Management Build Preparation - Added project build files for deployment - I also created a separate repository where the github workflow   will handle the project's deployment
      see: https://github.com/PSITS-UC-MAIN/psits-officemanagement

--- frontend/psits_frontend_officemanagement/src/App.jsx
+++ frontend/psits_frontend_officemanagement/src/App.jsx
@@ -1,19 +1,13 @@
 import './App.css'
 import DashboardUI from './components/Pages/DashboardUI'
 import LoginUI from './components/Pages/LoginUI'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import TimeLogUI from './components/Pages/TimeLogUI'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' index element={<DashboardUI />}/>
-          <Route path='timelog' element={<TimeLogUI />}/>
-          <Route path='login' element={<LoginUI />}/>
-        </Routes>
-      </BrowserRouter>
+      <Outlet />
     </>
   )
 }
--- frontend/psits_frontend_officemanagement/src/main.jsx
+++ frontend/psits_frontend_officemanagement/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import DashboardUI from './components/Pages/DashboardUI.jsx'
+import TimeLogUI from './components/Pages/TimeLogUI.jsx'
+import LoginUI from './components/Pages/LoginUI.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/psits-officemanagement/',
+    element: <App />,
+    children: [
+      {
+        path: '/psits-officemanagement/',
+        element: <DashboardUI />
+      },
+      {
+        path: '/psits-officemanagement/timelog',
+        element: <TimeLogUI />
+      },
+      {
+        path: '/psits-officemanagement/login',
+        element: <LoginUI />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
