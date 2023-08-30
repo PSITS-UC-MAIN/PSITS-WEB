8dab69b1a91ff0edc0d1ae3a54a1c93433380e60
Keeeyaan <keanjieden@gmail.com>
2023-08-30 22:46:25 +0800

    Added api route for user

---
+++ frontend/psits_frontend/src/api/user.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export async function getCurrentUser() {
+  const response = await axios.get(`user/current-user`, { withCredentials: true });
+  return response.data.user;
+}
