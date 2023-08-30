96a454cbb5a61a7380ba01812f64105e349d12e4
Keeeyaan <keanjieden@gmail.com>
2023-08-30 15:24:45 +0800

    [BACKEND] Added validation on register & login route

--- backend/psits_backend/src/routes/authRouter.js
+++ backend/psits_backend/src/routes/authRouter.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 const router = Router();
 import { login, logout, register } from "../controllers/authController.js";
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from "../middlewares/validationMiddleware.js";
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register", validateRegisterInput, register);
+router.post("/login", validateLoginInput, login);
 router.get("/logout", logout);
 
 export default router;
