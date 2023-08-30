49c1940ce6eed91c7408fcc5380815c22764478c
JayMar921 <jayharronabejar@gmail.com>
2023-08-30 20:12:53 +0800

    [BACKEND] API Validation - User Route - Added validateUserParam and validateUserBodyPatch for get, patch and delete routes

--- backend/psits_backend/src/routes/userRouter.js
+++ backend/psits_backend/src/routes/userRouter.js
@@ -7,13 +7,17 @@ import {
   updateUserbyId,
   deleteUserbyId,
 } from "../controllers/userController.js";
+import {
+  validateUserBodyPatch,
+  validateUserParam,
+} from "../middlewares/validations/userValidation.js";
 
 router.route("/current-user").get(getCurrentUser);
 router.route("/").get(getAllUser);
 router
   .route("/:userId")
-  .get(getUserbyId)
-  .patch(updateUserbyId)
-  .delete(deleteUserbyId);
+  .get(validateUserParam, getUserbyId)
+  .patch(validateUserParam, validateUserBodyPatch, updateUserbyId)
+  .delete(validateUserParam, deleteUserbyId);
 
 export default router;
