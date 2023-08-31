eaae22bd1fc6f1414ed4420f9b9bae76dbdf2e90
Keeeyaan <keanjieden@gmail.com>
2023-09-01 00:22:52 +0800

    Added prevent unauthorize access to admin page

--- frontend/psits_frontend/src/pages/admin/AdminLayout.tsx
+++ frontend/psits_frontend/src/pages/admin/AdminLayout.tsx
@@ -1,9 +1,24 @@
 import { Outlet } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 import AdminTopbar from "@/components/Topbar";
 import AdminSidebar from "@/components/Sidebar";
+import useStore from "@/store";
+import { useEffect } from "react";
 
 const AdminLayout = () => {
+  const navigate = useNavigate();
+  const store = useStore();
+
+  useEffect(() => {
+    if (!store.authUser?.isAdmin) {
+      toast.error("You are not authorize to access that site.");
+      navigate("/");
+      return;
+    }
+  }, []);
+
   return (
     <section className="flex h-screen">
       <AdminSidebar />
