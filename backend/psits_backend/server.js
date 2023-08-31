11d428ce236018afbadb1e86af6f6c848c34d34b
JayMar921 <jayharronabejar@gmail.com>
2023-08-31 17:19:08 +0800

    Updated build configs

--- backend/psits_backend/server.js
+++ backend/psits_backend/server.js
@@ -40,7 +40,11 @@ app.use(compression()); // compresses all routes
 // cors
 app.use(
   cors({
-    origin: ["http://localhost:5173", ...config.getCorsOrigin()],
+    origin: [
+      "http://localhost:5173",
+      "https://psits-uc-main.github.io/",
+      ...config.getCorsOrigin(),
+    ],
     credentials: true,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
