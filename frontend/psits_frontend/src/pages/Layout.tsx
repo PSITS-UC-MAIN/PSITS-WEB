a3fa06dbc0516bc5e05a854275389c8c77588f29
Keeeyaan <keanjieden@gmail.com>
2023-09-01 00:27:04 +0800

    Added query current user

--- frontend/psits_frontend/src/pages/Layout.tsx
+++ frontend/psits_frontend/src/pages/Layout.tsx
@@ -3,12 +3,34 @@ import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ContextProvider } from "@/components/Context";
+import { useQuery } from "@tanstack/react-query";
+import { getCurrentUser } from "@/api/user";
+import useStore from "@/store";
 
 const Layout = () => {
+  const store = useStore();
+
+  const {} = useQuery(["getCurrentUser"], getCurrentUser, {
+    refetchOnWindowFocus: false,
+    select(data) {
+      return data;
+    },
+    onSuccess(data) {
+      store.setAuthUser(data);
+      store.setRequestLoading(false);
+    },
+    onError(error) {
+      store.setRequestLoading(false);
+      console.log(error);
+    },
+  });
+
+  const user = store.authUser;
+
   return (
     <ContextProvider>
       <div className="flex flex-col">
-        <Header />
+        <Header data={user} />
         <div className="min-h-screen">
           <Outlet />
         </div>
