1bee1812f7f0ed4a997920001925befb3e90fb19
Keeeyaan <keanjieden@gmail.com>
2023-09-01 21:41:34 +0800

    Added all user api routes

--- frontend/psits_frontend/src/api/user.ts
+++ frontend/psits_frontend/src/api/user.ts
@@ -1,6 +1,26 @@
 import axios from "axios";
 
 export async function getCurrentUser() {
-  const response = await axios.get(`user/current-user`, { withCredentials: true });
+  const response = await axios.get(`user/current-user`);
   return response.data.user;
+}
+
+export async function getAllUser() {
+  const response = await axios.get(`user`);
+  return response.data.users;
+}
+
+export async function getUserbyId(userId: string) {
+  const response = await axios.get(`user/${userId}`);
+  return response.data;
+}
+
+export async function updateUserbyId(userId: string, data: any) {
+  const response = await axios.post(`user/${userId}`, data);
+  return response.data;
+}
+
+export async function deleteUserbyId(userId: string) {
+  const response = await axios.get(`user/${userId}`);
+  return response.data;
 }
