7a3978915084edd8d626e05daf5d6a4c1d3dac04
gochuicod <gochuicodarelleq@gmail.com>
2023-09-01 20:32:27 +0800

    Working Version of Shopping Cart

--- frontend/psits_frontend/src/components/Header.tsx
+++ frontend/psits_frontend/src/components/Header.tsx
@@ -12,12 +12,16 @@ import {
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { ShoppingCart, Trash } from "lucide-react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { useShoppingCart } from "./Context";
 import Profile from "./ProfileAvatar";
+import useShoppingCart from "@/store/useShoppingCart";
 import HamburgerMenu from "./HamburgerMenu";
 
 const Header = ({ data }: any) => {
-  const { cartItems, getTotalPrice, removeFromCart, decreaseQuantity, increaseQuantity } = useShoppingCart();
+  const {
+    cartItems,
+    totalPrice,
+    removeFromCart
+  } = useShoppingCart()
 
   return (
     <header className="sticky top-0 z-50">
@@ -80,16 +84,16 @@ const Header = ({ data }: any) => {
                   <ShoppingCart color="#fff" />
                   <span
                     className={
-                      Object.values(cartItems).length < 1
+                      cartItems.length < 1
                         ? "absolute top-[-5px] end-[-5px] bg-red-600 text-white font-bold rounded-full p-1 text-[15px] px-3 hidden"
                         : "absolute top-[-5px] end-[-5px] bg-red-600 text-white font-bold rounded-full p-1 text-[15px] px-3"
                     }
                   >
-                    {Object.values(cartItems).length}
+                    {cartItems.length}
                   </span>
                 </Button>
               </DialogTrigger>
-              <DialogContent className="sm:max-w-[70%] bg-[#f9f9f9]">
+              <DialogContent className="max-w-[85%] bg-[#f9f9f9]">
                 <div className="flex flex-col mx-[3%]">
                   <div className="grid grid-cols-6 text-center bg-[#254872] text-white rounded-md shadow-md p-5 mt-5 mb-5">
                     <span>Product</span>
@@ -100,33 +104,25 @@ const Header = ({ data }: any) => {
                     <span>Action</span>
                   </div>
                   <ScrollArea className="h-[30em] w-full rounded-md">
-                    {Object.values(cartItems).map((item) => (
+                    {cartItems.map((item) => (
                       <div
-                        key={item.id}
+                        key={item._id}
                         className="grid grid-cols-6 bg-white rounded-md shadow-md p-5 items-center mb-5"
                       >
                         <div className="flex flex-cols gap-x-5 items-center">
                           <img
-                            src={item.photo_img_link}
+                            src={item.photo_img_links[0]}
                             alt="Product Image"
                             className="w-[100px] h-[100px] rounded-md"
                           />
                           <span>{item.title}</span>
                         </div>
-                        <span className="text-center">{item.size}</span>
-                        <span className="text-center">{item.color}</span>
+                        <span className="text-center">{item.size === "" ? "Not Specified" : item.size}</span>
+                        <span className="text-center">{item.color === "" ? "Not Specified" : item.color}</span>
                         <span className="text-center">&#8369;{item.price}</span>
-                        <div className="grid grid-cols-3 items-center justify-items-center">
-                          <Button className="bg-[#268EA7] hover:bg-[#3da7c2]" onClick={() => decreaseQuantity(item.id)}>
-                            -
-                          </Button>
-                          <span className="text-center">{item.quantity}</span>
-                          <Button className="bg-[#268EA7] hover:bg-[#3da7c2]" onClick={() => increaseQuantity(item.id)}>
-                            +
-                          </Button>
-                        </div>
+                        <span className="text-center">{item.quantity}</span>
                         <div className="flex flex-row justify-center">
-                          <Button className="bg-red-600" onClick={() => removeFromCart(item.id)}>
+                          <Button className="bg-red-600" onClick={() => removeFromCart(item)}>
                             <Trash size={20} />
                           </Button>
                         </div>
@@ -135,7 +131,7 @@ const Header = ({ data }: any) => {
                   </ScrollArea>
                   <div className="flex flex-row justify-end items-center gap-x-10 p-5">
                     <span className="bg-white rounded-md py-2 px-5 shadow-md">
-                      Order Total:&emsp;&#8369;{getTotalPrice()}
+                      Order Total:&emsp;&#8369;{totalPrice}
                     </span>
                     <Button type="submit" className="bg-[#268EA7] hover:bg-[#3da7c2]">
                       Checkout
--- frontend/psits_frontend/src/pages/Merchandise.tsx
+++ frontend/psits_frontend/src/pages/Merchandise.tsx
@@ -1,115 +1,62 @@
 import { ShoppingBag } from "lucide-react";
+import { z } from "zod";
 
 import Wrapper from "@/components/Wrapper";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useShoppingCart } from "@/components/Context";
+import { getMerchandise } from "@/api/merchandise";
+import useShoppingCart from "@/store/useShoppingCart";
+import { useQuery } from "@tanstack/react-query";
 
-const dummyData = [
-  {
-    id: 1,
-    title: "Tiger",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 100,
-    discount: 10,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-  {
-    id: 2,
-    title: "Slant Hoodie",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 750,
-    discount: 150,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-  {
-    id: 3,
-    title: "Slant Hoodie",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 50,
-    discount: 0,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-  {
-    id: 4,
-    title: "Slant Hoodie",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 400,
-    discount: 50,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-  {
-    id: 5,
-    title: "Slant Hoodie",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 300,
-    discount: 15,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-  {
-    id: 6,
-    title: "Slant Hoodie",
-    createdBy: 1234,
-    information: "Cool T-shirt",
-    price: 150,
-    discount: 5,
-    stock: 20,
-    photo_img_link: "",
-    size: "Large",
-    color: "Black",
-    rating: 20,
-    quantity: 1,
-    showPublic: true,
-  },
-];
+interface Merchandise {
+  _id: string,
+  title: string,
+  information: string,
+  price: number,
+  discount: number,
+  stock: number,
+  photo_img_links: [],
+  size: string,
+  color: string,
+  styles: [],
+  rating: number,
+  showPublic: boolean
+}
+
+const MerchandiseSchema = z
+  .object({
+    _id: z.string(),
+    title: z.string(),
+    information: z.string(),
+    price: z.number(),
+    discount: z.number(),
+    stock: z.number(),
+    photo_img_links: z.string(),
+    size: z.string(),
+    color: z.string(),
+    rating: z.number(),
+    quantity: z.number(),
+    showPublic: z.boolean()
+  })
+
+export type MerchandiseSchema = z.infer<typeof MerchandiseSchema>;
 
 const Merchandise = () => {
-  const { addToCart } = useShoppingCart();
+  const { addToCart } = useShoppingCart()
+  const { data } = useQuery({
+    queryKey: ["merch"],
+    queryFn: getMerchandise
+  })
 
   return (
     <Wrapper title="PSITS | Merchandise">
       <div className="min-h-screen my-20">
         <h1 className="text-7xl text-center font-bold text-[#1A1A1A] mb-20">Merchandise</h1>
         <div className="flex flex-row flex-wrap justify-center">
-          {dummyData.map((item) => (
-            <Card key={item.id} className="w-[350px] border-0 shadow-none">
+          {data?.merchandise?.map((item: any) => (
+            <Card key={item._id} className="w-[350px] border-0 shadow-none">
               <CardHeader className="relative">
-                <div className="w-full h-[400px] bg-gray-200 rounded-lg shadow-lg"></div>
+                <img src={item.photo_img_links[0]} alt="Merch Item" className="w-full h-[400px] rounded-lg shadow-lg" />
                 <Button
                   className="bg-[#000] bg-opacity-100 hover:bg-[#000] hover:bg-opacity-75 py-[7.5%] absolute bottom-8 end-8 rounded-full"
                   onClick={() => addToCart(item)}
@@ -128,7 +75,7 @@ const Merchandise = () => {
         </div>
       </div>
     </Wrapper>
-  );
-};
+  )
+}
 
 export default Merchandise;
