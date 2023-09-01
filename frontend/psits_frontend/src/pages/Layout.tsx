b4d66465ce9c2c39fa9976a320822e1284423f1b
gochuicod <gochuicodarelleq@gmail.com>
2023-09-01 13:11:44 +0800

    Removed Context Provider

--- frontend/psits_frontend/src/pages/Layout.tsx
+++ frontend/psits_frontend/src/pages/Layout.tsx
@@ -2,19 +2,16 @@ import { Outlet } from "react-router-dom";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ContextProvider } from "@/components/Context";
 
 const Layout = () => {
   return (
-    <ContextProvider>
-      <div className="flex flex-col">
-        <Header />
-        <div className="min-h-screen">
-          <Outlet />
-        </div>
-        <Footer />
+    <div className="flex flex-col">
+      <Header />
+      <div className="min-h-screen">
+        <Outlet />
       </div>
-    </ContextProvider>
+      <Footer />
+    </div>
   );
 };
 
