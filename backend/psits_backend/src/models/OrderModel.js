d435ce5fc6d2d8d95b818ac323dae6b921b63fe6
gochuicod <gochuicodarelleq@gmail.com>
2023-09-04 21:33:26 +0800

    Updated Order Model

--- backend/psits_backend/src/models/OrderModel.js
+++ backend/psits_backend/src/models/OrderModel.js
@@ -1,66 +1,41 @@
 import mongoose from "mongoose";
-import { GenerateReference } from "../utils/ServerUtils.js";
 const Schema = mongoose.Schema;
 
 const OrderModel = new mongoose.Schema({
-  student_id: {
+  userId: {
     type: Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  merch_id: {
-    type: Schema.Types.ObjectId,
-    ref: "Merchandise",
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  reference: {
+  orderItems: [
+    {
+      merchId: {
+        type: Schema.Types.ObjectId,
+        ref: "Merchandise",
+        required: true,
+      },
+      price: {
+        type: String,
+        require: true,
+      },
+      quantity: {
+        type: Number,
+        require: true,
+      },
+      size: {
+        type: String,
+        require: true,
+      },
+      color: String,
+    },
+  ],
+  orederStatus: {
     type: String,
-    required: false,
-    default: GenerateReference(),
+    enum: ["CANCELLED", "PENDING", "CLAIMED"],
+    default: "PENDING",
   },
-  information: {
+  additionalInfo: {
     type: String,
-    required: true,
-  },
-  size: {
-    type: String,
-    required: false,
-    default: "N/A",
-  },
-  color: {
-    type: String,
-    required: false,
-    default: "N/A",
-  },
-  style: {
-    type: String,
-    required: false,
-    default: "N/A",
-  },
-  status: {
-    type: String,
-    required: false,
-    default: "ORDERED",
-  },
-  review: {
-    type: String,
-    required: false,
-    default: "",
-  },
-  rating: {
-    type: Number,
-    required: false,
-    default: 0,
-  },
-  quantity: {
-    type: Number,
-    required: false,
-    default: 1,
   },
 });
 
