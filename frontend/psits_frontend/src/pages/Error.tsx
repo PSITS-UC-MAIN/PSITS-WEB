56d2038775ee9a920cdf0e0c97139734b44e27ba
Keeeyaan <keanjieden@gmail.com>
2023-09-05 23:26:42 +0800

    Return both error ui

--- frontend/psits_frontend/src/pages/Error.tsx
+++ frontend/psits_frontend/src/pages/Error.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 
 const Error = () => {
   const error = useRouteError();
-
+  
   if (isRouteErrorResponse(error)) {
     return (
       <Wrapper title="PSITS | Page Not Found">
@@ -26,8 +26,16 @@ const Error = () => {
   return (
     <Wrapper title="PSITS | Page Not Found">
       <div>
-        <p>Something went wrong</p>
-        <Link to="/">Back Home</Link>
+        <div className="text-center flex items-center flex-col justify-center h-screen">
+          <img className="md:max-w-[600px] block mb-8" src={notfound} alt="not-found" />
+          <h3 className="text-xl md:text-3xl font-bold mb-4">Ohh! Page Not Found</h3>
+          <p className="text-gray-500 mb-2">we can't seem to find the page you are looking for</p>
+          <Link to="/" className="">
+            <Button variant="link" className="text-[#074873] text-lg">
+              Back Home
+            </Button>
+          </Link>
+        </div>
       </div>
     </Wrapper>
   );
