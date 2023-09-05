3634521e16ea420bd1755fa64b9966761d97f740
gochuicod <gochuicodarelleq@gmail.com>
2023-09-05 22:48:02 +0800

    Integrate single image upload with merch item

--- backend/psits_backend/src/controllers/merchandiseController.js
+++ backend/psits_backend/src/controllers/merchandiseController.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from "http-status-codes";
 import Merchandise from "../models/MerchandiseModel.js";
 import { UnauthorizedError } from "../errors/customErrors.js";
+import { v2 as cloudinary } from "cloudinary";
+import { promises as fs } from "fs";
 
 export const getMerchandise = async (req, res) => {
   const merchandise = await Merchandise.find({});
@@ -11,7 +13,22 @@ export const getMerchandise = async (req, res) => {
 export const createMerchandiseItem = async (req, res) => {
   if (!req.user.isAdmin) throw new UnauthorizedError("Unauthorized!");
 
-  await Merchandise.create(req.body);
+  let newBody = { ...req.body };
+  
+  if (req.file) {
+    newBody = JSON.parse(req.body.merch);
+    
+    // upload the image to cloudinary
+    const response = await cloudinary.uploader.upload(req.file.path);
+    // delete the image in the public folder
+    await fs.unlink(req.file.path);
+    newBody.images = [{
+      image: response.secure_url,
+      imagePublicId: response.public_id
+    }]
+  }
+
+  await Merchandise.create(newBody);
   res.status(StatusCodes.OK).json({ msg: "Merchandise Item Created!" })
 }
 
