ece99cf3150a235695eae5d6a3b47fcee5f4d3ab
Keeeyaan <keanjieden@gmail.com>
2023-09-05 23:23:30 +0800

    Removed currentUser query

--- frontend/psits_frontend/src/pages/Layout.tsx
+++ frontend/psits_frontend/src/pages/Layout.tsx
@@ -2,33 +2,11 @@ import { Outlet } from "react-router-dom";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useQuery } from "@tanstack/react-query";
-import { getCurrentUser } from "@/api/user";
-import useStore from "@/store";
 
 const Layout = () => {
-  const store = useStore();
-
-  const {} = useQuery(["getCurrentUser"], getCurrentUser, {
-    refetchOnWindowFocus: false,
-    select(data) {
-      return data;
-    },
-    onSuccess(data) {
-      store.setAuthUser(data);
-      store.setRequestLoading(false);
-    },
-    onError(error) {
-      store.setRequestLoading(false);
-      console.log(error);
-    },
-  });
-
-  const user = store.authUser;
-
   return (
     <div className="flex flex-col">
-      <Header data={user} />
+      <Header />
       <div className="min-h-screen">
         <Outlet />
       </div>
