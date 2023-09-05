a0cd5444ab727b12c330dc47ea4d9b6abbfd3a63
Keeeyaan <keanjieden@gmail.com>
2023-09-05 23:21:20 +0800

    Added forgot password api route

--- frontend/psits_frontend/src/api/auth.ts
+++ frontend/psits_frontend/src/api/auth.ts
@@ -9,6 +9,16 @@ export async function loginUser(data: any) {
   const response = await axios.post(`auth/login`, data, { withCredentials: true });
   return response.data;
 }
+export async function forgotPassword(userId: string) {
+  const response = await axios.post(`auth/forgot-password/${userId}`);
+  return response.data;
+}
+
+export async function resetPassword({ userId, token, data }: { userId: string; token: string; data: any }) {
+  const response = await axios.post(`auth/reset-password/${userId}/${token}`, data);
+  return response.data;
+}
+
 export async function logoutUser() {
   const response = await axios.get(`auth/logout`, { withCredentials: true });
   return response.data;
