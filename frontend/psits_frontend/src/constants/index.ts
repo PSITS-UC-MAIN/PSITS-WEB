9698d890391e00314f24391b6cba70df0b93fa9c
Keeeyaan <keanjieden@gmail.com>
2023-09-06 22:21:00 +0800

    Added highlight images & minor changes

--- frontend/psits_frontend/src/constants/index.ts
+++ frontend/psits_frontend/src/constants/index.ts
@@ -1,4 +1,17 @@
-import { psits_banner, psits_banner2, ucDeanAnnouncementBanner } from "@/assets";
+import {
+  psits_banner,
+  psits_banner2,
+  ucDeanAnnouncementBanner,
+  psits_officers,
+  highlight1,
+  highlight2,
+  highlight3,
+  highlight4,
+  highlight5,
+  highlight6,
+  highlight7,
+  highlight8,
+} from "@/assets";
 
 export const homeBannerImages = [
   {
@@ -15,38 +28,118 @@ export const homeBannerImages = [
   },
 ];
 
-export const aboutBannerImages = [
+export const aboutUsBannerImages = [
   {
     name: "psits_banner",
-    banner: psits_banner,
-  },
-  {
-    name: "psits_banner2",
-    banner: psits_banner2,
-  },
-  {
-    name: "psits_banner3",
-    banner: psits_banner2,
+    banner: psits_officers,
   },
 ];
 
-export const alumniShoutoutBannerImages = [
+export const aboutUsHighlights = [
   {
-    name: "psits_banner",
-    banner: psits_banner,
+    id: "1",
+    image: highlight1,
   },
   {
-    name: "psits_banner2",
-    banner: psits_banner2,
+    id: "2",
+    image: highlight2,
   },
   {
-    name: "psits_banner3",
-    banner: psits_banner,
+    id: "3",
+    image: highlight3,
+  },
+  {
+    id: "4",
+    image: highlight4,
+  },
+  {
+    id: "5",
+    image: highlight5,
+  },
+  {
+    id: "6",
+    image: highlight6,
+  },
+  {
+    id: "7",
+    image: highlight7,
+  },
+  {
+    id: "8",
+    image: highlight8,
   },
 ];
 
-export default {
-  homeBannerImages,
-  aboutBannerImages,
-  alumniShoutoutBannerImages,
-};
+export const psitsOfficers = [
+  {
+    name: "Jeshelle Andrea Colonia",
+    position: "President",
+  },
+  {
+    name: "Clint Louie Tuyor",
+    position: "VP-Internal",
+  },
+  {
+    name: "Vince Andrew Santoya",
+    position: "VP-External",
+  },
+  {
+    name: "Dexie Ann Cabido",
+    position: "Treasurer",
+  },
+  {
+    name: "Angela Postrero",
+    position: "Asst. Treasurer",
+  },
+  {
+    name: "Aubrey Leyros",
+    position: "Auditor",
+  },
+  {
+    name: "Daisy Lyn Laygan",
+    position: "P.I.O",
+  },
+  {
+    name: "John Paul Costillas",
+    position: "P.R.O",
+  },
+  {
+    name: "Cyril John Ypil",
+    position: "Chief Volunteer",
+  },
+  {
+    name: "Edrian Laro",
+    position: "4th Year Rep",
+  },
+  {
+    name: "Maria Clemens Pia Abellana",
+    position: "3rd Year Rep",
+  },
+  {
+    name: "Princess Villanueva",
+    position: "2nd Year Rep",
+  },
+];
+
+export const psitsWebDevelopers = [
+  {
+    id: 1,
+    name: "Jayharron Mar Abejar",
+    position: "Project Owner",
+  },
+  {
+    id: 2,
+    name: "Kean Jieden Villaflor",
+    position: "Team Lead",
+  },
+  {
+    id: 3,
+    name: "Darelle Gochuico",
+    position: "Dev",
+  },
+  {
+    id: 3,
+    name: "Maria Clemens Pia Abellana",
+    position: "Q/A",
+  },
+];
