40e45424dbf2c1a4795808b76a707654c9d30531
gochuicod <gochuicodarelleq@gmail.com>
2023-09-08 17:41:49 +0800

    Added new features
    Animations for queries were added, changed defaults and fixed some
    bugs along the way

--- frontend/psits_frontend/src/components/merchandise/merchandiseCard.tsx
+++ frontend/psits_frontend/src/components/merchandise/merchandiseCard.tsx
@@ -1,8 +1,7 @@
 import { Card, CardContent, CardHeader } from "../ui/card";
-import { FileEdit, Plus, ShoppingBag, Trash } from "lucide-react";
+import { FileEdit, Loader2, Plus, ShoppingBag, Trash } from "lucide-react";
 import { deleteMerchandiseItem, updateMerchandiseItem } from "@/api/merchandise";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import useShoppingCart from "@/store/useShoppingCart";
 import { toast } from "react-toastify";
 import { Button } from "../ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from "../ui/dialog";
@@ -15,6 +14,8 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import useStore from "@/store";
 import { useState } from "react";
+import { addToCart } from "@/api/cart";
+import { Slide } from "react-slideshow-image";
 
 interface MerchandiseCardProps {
   item: {
@@ -42,25 +43,29 @@ const MerchandiseSchema = z.object({
   discount: z.number().or(z.nan()).default(Number.NaN),
   images: z.any(),
   color: z.string(),
+  size: z.string()
 });
 
 type MerchandiseSchema = z.infer<typeof MerchandiseSchema>;
 
 const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
-  const { addToCart } = useShoppingCart();
   const queryClient = useQueryClient();
   const store = useStore();
   const [file, setFile] = useState("");
 
-  const { mutate: deleteMutate, reset: deleteReset } = useMutation({
+  const {
+    mutate: deleteMutate,
+    reset: deleteReset,
+    isLoading: deleteIsLoading
+  } = useMutation({
     mutationFn: deleteMerchandiseItem,
     onSuccess: (merch) => {
       queryClient.invalidateQueries(["merch"]);
-      toast.success(`${merch.message}`);
+      toast.success(`${merch.message}`, { position: 'bottom-right' });
       deleteReset();
     },
     onError(error: any) {
-      toast.error(error.response.merch.message || error.message);
+      toast.error(error.response.merch.message || error.message, { position: 'bottom-right' });
     },
   });
 
@@ -71,43 +76,52 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
   } = useForm<MerchandiseSchema>({
     resolver: zodResolver(MerchandiseSchema),
     defaultValues: {
-      price: 0,
-      discount: 0,
-      color: ""
+      name: item.name,
+      price: item.price,
+      discount: item.discount,
+      color: item.color,
+      description: item.description
     }
   });
 
-  const { mutate: updateMutate, reset: updateReset } = useMutation({
+  const {
+    mutate: updateMutate,
+    reset: updateReset,
+    isLoading: updateIsLoading
+  } = useMutation({
     mutationFn: updateMerchandiseItem,
     onSuccess: (merch) => {
       queryClient.invalidateQueries(["merch"]);
-      toast.success(`${merch.message}`);
+      toast.success(`${merch.message}`, { position: 'bottom-right' });
       updateReset();
     },
     onError(error: any) {
-      toast.error(error.response.merch.message || error.message);
+      toast.error(error.response.merch.message || error.message, { position: 'bottom-right' });
     },
   });
 
   const onSubmit: SubmitHandler<MerchandiseSchema> = (data: any) => {
     const formData = new FormData();
     const merchandiseItemId = item._id;
-    console.log(data);
-    
 
     if (data.images.length > 0) {
       for(let i = 0; i < data.images.length;i++){
-        formData.append("images",data.images[i]);
-        formData.append("image", item.images[i].image);
-        formData.append("imagePublicId", item.images[i].imagePublicId);
+        formData.append("images[]",data.images[i]);
+
+        if (item.images[i]) {
+          formData.append("image[]", item.images[i].image);
+          formData.append("imagePublicId[]", item.images[i].imagePublicId);
+        }
       }
       formData.append("merch", JSON.stringify(data));
       data = formData
       updateMutate({ merchandiseItemId, data });
     } else {
       for(let i = 0; i < item.images.length;i++){
-        formData.append("oldImage", item.images[i].image);
-        formData.append("oldImagePublicId", item.images[i].imagePublicId);
+        if (item.images[i]) {
+          formData.append("image[]", item.images[i].image);
+          formData.append("imagePublicId[]", item.images[i].imagePublicId);
+        }
       }
       formData.append("merch", JSON.stringify(data));
       data = formData
@@ -115,31 +129,68 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
     }
   };
 
+  const { mutate: createMutate } = useMutation({
+    mutationFn: addToCart,
+    onSuccess: (cart) => {
+      queryClient.invalidateQueries(["cart"]);
+      toast.success(`${cart.msg}`, { position: 'bottom-right' });
+    },
+    onError(error: any) {
+      toast.error(error.response.cart.message || error.message, { position: 'bottom-right' })
+    }
+  })
+
+  const handleAddToCart = () => {
+    const userId = Number(store.authUser?.userId)
+    const size = item.size.split(',')
+    const color = item.color.split(',')
+    const data = {
+      merchId: item._id,
+      price: item.price,
+      quantity: 1,
+      size: size[0],
+      color: color[0],
+      image: item.images[0].image,
+      name: item.name,
+    }
+    
+    createMutate({userId, data})
+  }
+
   return (
     <Card className="w-[350px] border-0 shadow-none">
       <CardHeader className="relative">
-        <img
-          src={item.images[0].image}
-          alt="Merch Item"
-          className="w-full h-[400px] rounded-lg border-2 border-black"
-        />
+        {item?.images.length > 1 ?
+          <Slide indicators>
+            {item?.images.map((slideImage) => (
+              <div
+                key={slideImage.imagePublicId}
+                className="flex items-center justify-center rounded h-[400px] bg-contain bg-no-repeat bg-center border-2"
+                style={{ backgroundImage: `url(${slideImage.image})` }}
+              />
+            ))}
+          </Slide>
+          :
+          <img src={item.images[0].image} alt="Product Image" className="h-[400px] shadow-lg rounded-lg" />
+        }
         {store.authUser?.isAdmin && (
           <>
             <Button
               className="bg-red-600 hover:bg-red-500 py-[7.5%] absolute top-[0%] end-[0%] rounded-full"
               onClick={() => deleteMutate(item._id)}
+              disabled={deleteIsLoading}
             >
               <Trash size={20} />
             </Button>
             <Dialog>
               <DialogHeader>
                 <DialogTrigger asChild>
-                  <Button className="bg-[#268EA7] hover:bg-[#3da7c2] py-[7.5%] absolute bottom-[23%] end-[11%] rounded-full">
+                  <Button className="bg-[#268EA7] hover:bg-[#3da7c2] py-[7.5%] absolute top-[15%] end-[0%] rounded-full">
                     <FileEdit size={20} />
                   </Button>
                 </DialogTrigger>
               </DialogHeader>
-              <DialogContent className="h-[85%] bg-white mx-10">
+              <DialogContent className="h-[85%] w-[1000px] bg-white mx-10">
                 <ScrollArea>
                   <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
                     <div className="flex flex-col mt-10 gap-y-10 items-center mx-5">
@@ -200,6 +251,38 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
                       </div>
                       <div className="flex flex-row gap-x-5">
                         <div className="flex flex-col gap-y-3">
+                          <Label className="text-gray-500" htmlFor="itemSize">
+                            Item Size
+                          </Label>
+                          <Input
+                            autoComplete="off"
+                            id="itemSize"
+                            placeholder="Enter item size"
+                            defaultValue={item.size}
+                            {...register("size")}
+                          />
+                          {errors.size && (
+                            <p className="text-red-400 text-sm font-light">{errors.size.message}</p>
+                          )}
+                        </div>
+                        <div className="flex flex-col gap-y-3">
+                          <Label className="text-gray-500" htmlFor="itemColor">
+                            Item Color
+                          </Label>
+                          <Input
+                            autoComplete="off"
+                            id="itemColor"
+                            placeholder="Enter item color"
+                            defaultValue={item.color}
+                            {...register("color")}
+                          />
+                          {errors.color && (
+                            <p className="text-red-400 text-sm font-light">{errors.color.message}</p>
+                          )}
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center gap-x-5">
+                        <div className="flex flex-col gap-y-3">
                           <Label className="text-gray-500" htmlFor="itemDiscount">
                             Item Discount in %
                           </Label>
@@ -216,20 +299,6 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
                           )}
                         </div>
                         <div className="flex flex-col gap-y-3">
-                          <Label className="text-gray-500" htmlFor="itemColor">
-                            Item Color
-                          </Label>
-                          <Input
-                            autoComplete="off"
-                            id="itemColor"
-                            placeholder="Enter item color"
-                            defaultValue={item.color}
-                            {...register("color")}
-                          />
-                        </div>
-                      </div>
-                      <div className="flex flex-row items-center gap-x-5">
-                        <div className="flex flex-col gap-y-3">
                           <Label className="text-gray-500" htmlFor="itemDesc">
                             Item Description
                           </Label>
@@ -244,8 +313,8 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
                           )}
                         </div>
                       </div>
-                      <Button type="submit" className="w-full">
-                        Update
+                      <Button type="submit" className="w-full" disabled={updateIsLoading}>
+                        {updateIsLoading ? <Loader2 className=" animate-spin" /> : "Update"}
                       </Button>
                     </div>
                   </form>
@@ -256,8 +325,8 @@ const MerchandiseCard = ({ item }: MerchandiseCardProps) => {
         )}
         {store.authUser != null && (
           <Button
-            className="bg-black hover:bg-[#303030] py-[7.5%] absolute bottom-[9%] end-[11%] rounded-full"
-            onClick={() => addToCart(item)}
+            className={item.images.length > 1 ? "bg-black hover:bg-[#303030] py-[7.5%] absolute bottom-[15%] end-[11%] rounded-full" : "bg-black hover:bg-[#303030] py-[7.5%] absolute bottom-[9%] end-[11%] rounded-full"}
+            onClick={handleAddToCart}
           >
             <ShoppingBag size={20} />
           </Button>
