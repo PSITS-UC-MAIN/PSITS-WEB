53b4694a1c186f96079acbda17c108938bb31cdb
Keeeyaan <keanjieden@gmail.com>
2023-09-09 20:46:39 +0800

    Added delete confirmation & edit functionality

--- frontend/psits_frontend/src/components/announcements/AnnoucementCard.tsx
+++ frontend/psits_frontend/src/components/announcements/AnnoucementCard.tsx
@@ -1,15 +1,41 @@
 import { format, parseISO } from "date-fns";
 import ReackMarkDown from "react-markdown";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-import { deleteAnnouncement } from "@/api/announcement";
+import { deleteAnnouncementById, updateAnnouncementById } from "@/api/announcement";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar";
-import { FileEdit, Trash2, User } from "lucide-react";
+import { FileEdit, Loader2, Trash2, User } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "../ui/button";
 import useStore from "@/store";
-import { toast } from "react-toastify";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 
 interface AnnouncementCardProps {
   id: string;
@@ -18,8 +44,15 @@ interface AnnouncementCardProps {
   author: string;
   content: string;
   image: string;
-  authorImage: string
+  authorImage: string;
 }
+
+const AnnouncementSchema = z.object({
+  title: z.string().min(4),
+  content: z.string().min(4),
+});
+
+type AnnouncementSchema = z.infer<typeof AnnouncementSchema>;
 
 const AnnoucementCard = ({ id, title, author, content, creationDate, image, authorImage }: AnnouncementCardProps) => {
   const store = useStore();
@@ -27,15 +60,51 @@ const AnnoucementCard = ({ id, title, author, content, creationDate, image, auth
   const parseDate = creationDate.toLocaleString();
   const formattedDate = format(parseISO(parseDate), "PPP | pp");
 
-  const { mutate, reset } = useMutation({
-    mutationFn: deleteAnnouncement,
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset: resetUpdateForm,
+  } = useForm<AnnouncementSchema>({
+    resolver: zodResolver(AnnouncementSchema),
+  });
+
+  // update annoucement
+  const { mutate: update, isLoading: updateIsLoading } = useMutation({
+    mutationFn: updateAnnouncementById,
     onMutate() {
       store.setRequestLoading(true);
     },
     onSuccess: (data) => {
       queryClient.invalidateQueries(["announcements"]);
       store.setRequestLoading(false);
-      toast.success(`${data.message}!`);
+      toast.success(`${data.message}!`, {
+        position: "bottom-right",
+      });
+      resetUpdateForm();
+    },
+    onError(error: any) {
+      store.setRequestLoading(false);
+      toast.error(error.response.data.message || error.message);
+    },
+  });
+
+  const onEditSubmit: SubmitHandler<AnnouncementSchema> = (data) => {
+    update({ announcementId: id, data });
+  };
+
+  // delete announcement
+  const { mutate, reset } = useMutation({
+    mutationFn: deleteAnnouncementById,
+    onMutate() {
+      store.setRequestLoading(true);
+    },
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["announcements"]);
+      store.setRequestLoading(false);
+      toast.success(`${data.message}!`, {
+        position: "bottom-right",
+      });
       reset();
     },
     onError(error: any) {
@@ -63,12 +132,68 @@ const AnnoucementCard = ({ id, title, author, content, creationDate, image, auth
           </div>
           {store.authUser?.isAdmin && (
             <div className="flex items-center">
-              <Button variant="ghost">
-                <FileEdit size={16} />
-              </Button>
-              <Button onClick={() => mutate(id)} variant="ghost">
-                <Trash2 color="#df2020" size={16} />
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="ghost">
+                    <FileEdit size={16} />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <form onSubmit={handleSubmit(onEditSubmit)}>
+                    <DialogHeader>
+                      <DialogTitle>Edit Announcement</DialogTitle>
+                      <DialogDescription>
+                        Make changes to the announcement. Click save when you're done.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                      <div className="grid items-center gap-4">
+                        <Label htmlFor="title">Title</Label>
+                        <Input id="title" className="col-span-3" defaultValue={title} {...register("title")} />
+                        {errors.title && <p className="text-red-400 text-sm font-light">{errors.title.message}</p>}
+                      </div>
+                      <div className="grid items-center gap-4">
+                        <Label htmlFor="content">Content</Label>
+                        <Textarea
+                          id="content"
+                          rows={5}
+                          defaultValue={content}
+                          className="bg-[#fafafa]"
+                          {...register("content")}
+                        />
+                        {errors.content && <p className="text-red-400 text-sm font-light">{errors.content.message}</p>}
+                      </div>
+                    </div>
+                    <DialogFooter>
+                      <Button type="submit" className="bg-[#268EA7] hover:bg-[#3da7c2]" disabled={updateIsLoading}>
+                        {updateIsLoading ? <Loader2 className=" animate-spin" /> : "Save changes"}
+                      </Button>
+                    </DialogFooter>
+                  </form>
+                </DialogContent>
+              </Dialog>
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Button variant="ghost">
+                    <Trash2 color="#df2020" size={16} />
+                  </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete the annoucement and remove the data
+                      from the server.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={() => mutate(id)} className="bg-[#074873] hover:bg-[#2d7db3]">
+                      Continue
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </div>
           )}
         </div>
