896ea63625edef93a3af48c64190834a17e03ece
Keeeyaan <keanjieden@gmail.com>
2023-09-09 20:45:30 +0800

    Fixed naming variable

--- frontend/psits_frontend/src/api/announcement.ts
+++ frontend/psits_frontend/src/api/announcement.ts
@@ -9,12 +9,12 @@ export async function createAnnouncement(data: any) {
   const response = await axios.post(`announcement/`, data);
   return response.data;
 }
-export async function updateAnnouncement(announcementId: string, data: any) {
+export async function updateAnnouncementById({ announcementId, data }: { announcementId: string; data: any }) {
   const response = await axios.patch(`announcement/${announcementId}`, data);
   return response.data;
 }
 
-export async function deleteAnnouncement(announcementId: string) {
+export async function deleteAnnouncementById(announcementId: string) {
   const response = await axios.delete(`announcement/${announcementId}`);
   return response.data;
 }
