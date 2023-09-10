2acf5d1582244c7fb3668c0c093608ac7276ae1a
Keeeyaan <keanjieden@gmail.com>
2023-09-10 20:26:07 +0800

    Added admin routes page

---
+++ frontend/psits_frontend/src/pages/admin/AdminRoutes.tsx
@@ -0,0 +1,12 @@
+import Wrapper from "@/components/Wrapper";
+import { Maintenance } from "..";
+
+const AdminRoutes = () => {
+  return (
+    <Wrapper title="PSITS Admin | Routes" noMargin>
+      <Maintenance/>
+    </Wrapper>
+  );
+};
+
+export default AdminRoutes;
