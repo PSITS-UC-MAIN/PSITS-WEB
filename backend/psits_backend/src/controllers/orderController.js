129cfa876581cd896d034043167b04d40d9f10a7
Keeeyaan <Keanjieden@gmail.com>
2024-01-06 16:50:08 +0800

    added orderStatus for search & getOrderById API

--- backend/psits_backend/src/controllers/orderController.js
+++ backend/psits_backend/src/controllers/orderController.js
@@ -10,7 +10,10 @@ export const getAllOrders = async (req, res) => {
   const search = req.query.search || "";
 
   const orders = await Order.find({
-    $or: [{ orderId: { $regex: search, $options: "i" } }],
+    $or: [
+      { orderId: { $regex: search, $options: "i" } },
+      { orderStatus: { $regex: search, $options: "i" } },
+    ],
   })
     .skip(page * limit)
     .limit(limit)
@@ -19,7 +22,9 @@ export const getAllOrders = async (req, res) => {
 
   const total = await User.countDocuments({
     orderId: { $regex: search, $options: "i" },
+    orderStatus: { $regex: search, $options: "i" },
   });
+
   const response = {
     error: false,
     total,
@@ -29,6 +34,14 @@ export const getAllOrders = async (req, res) => {
   };
 
   res.status(StatusCodes.OK).json(response);
+};
+
+export const getOrderById = async (req, res) => {
+  if (!req.user.isAdmin) throw new UnauthorizedError("Unauthorized!");
+
+  const order = await Order.findOne({ orderId: req.params.orderId });
+
+  res.status(StatusCodes.OK).json({ order });
 };
 
 export const getCurrentUserOrders = async (req, res) => {
@@ -58,6 +71,7 @@ export const createOrder = async (req, res) => {
 
 export const updateOrder = async (req, res) => {
   const { orderStatus, additionalInfo, orderId } = req.body;
+
   const updatedOrder = await Order.findOne({ _id: orderId });
 
   if (orderStatus) updatedOrder.orderStatus = orderStatus;
