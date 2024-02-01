77ee768cb4626f799ffae97cefbb753147ca5d7d
gochuicod <gochuicodarelleq@gmail.com>
2024-02-01 19:17:20 +0800

    Changed default order status

--- backend/psits_backend/src/models/OrderModel.js
+++ backend/psits_backend/src/models/OrderModel.js
@@ -16,7 +16,7 @@ const OrderModel = new mongoose.Schema({
   orderStatus: {
     type: String,
     enum: ["ORDERED", "PENDING", "CLAIMED", "CANCELLED"],
-    default: "PENDING",
+    default: "ORDERED",
   },
   additionalInfo: {
     type: String,
